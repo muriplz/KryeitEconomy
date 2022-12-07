7432307e275efd181facdca22c68bc95bb9e2f09
muriplz <46825658+muriplz@users.noreply.github.com>
2022-12-07 23:34:22 +0100

    cambio de nombre del proyecto
    de "examplemod" a "kryeiteconomy"

--- src/main/java/com/kryeit/kryeiteconomy/KryeitEconomy.java
+++ src/main/java/com/kryeit/kryeiteconomy/KryeitEconomy.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.kryeit.kryeiteconomy;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.world.level.block.Block;
@@ -18,13 +18,14 @@ import org.slf4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
+@Mod("kryeiteconomy")
+public class KryeitEconomy
 {
+    private static final String ID = "kryeiteconomy";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public ExampleMod()
+    public KryeitEconomy()
     {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
